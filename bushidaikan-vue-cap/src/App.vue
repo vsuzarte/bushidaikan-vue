0de58cda8420103dc2d214c2488430f661e5f670
Vitor Suzarte <vitor.suzarte@ecalc.com.br>
2023-12-21 16:28:00 -0300

    vue router, vuetify, eslint

--- bushidaikan-vue-cap/src/App.vue
+++ bushidaikan-vue-cap/src/App.vue
@@ -1,33 +1,6 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col>
-        <v-card>
-          <v-card-title>Meu Primeiro Componente Vuetify</v-card-title>
-          <v-card-subtitle>Usando Vuetify no Vue.js</v-card-subtitle>
-          <v-card-text>
-            Bem-vindo ao Vuetify! Você pode começar a usar os componentes Vuetify aqui.
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+  <router-view></router-view>
 </template>
 
-<script>
-
-export default {
-  name: 'App',
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ bushidaikan-vue-cap/src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <div class="flex-container">
+        <div id="menu-container">
+            <img id="logo" src="" />
+            <h1>Karate-Do Bushidaikan</h1>
+            <button id="start-game-btn" class="btn btn-bushi">Quiz das posturas e técnias</button>
+            <button id="" class="btn btn-black">Quiz das palavras</button>
+        </div>
+
+        <div id="quiz-container">
+            <div id="question-container"></div>
+            <div id="options-container"></div>
+            <div id="result-container" style="display: none;"></div>
+            <button id="restart-btn" class="btn-question btn-reiniciar" style="display: none;">Reiniciar</button>
+            <button id="back-to-menu-btn" class="btn-question" style="display: none;">Voltar para o Início</button>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
