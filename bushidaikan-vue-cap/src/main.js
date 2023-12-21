7ef2695cbfde1f737e92b77f3e96ab469bb52157
Vitor Suzarte <vitor.suzarte@ecalc.com.br>
2023-12-21 18:21:44 -0300

    Tela de seleção de postura e inclusão dos icones

--- bushidaikan-vue-cap/src/main.js
+++ bushidaikan-vue-cap/src/main.js
@@ -6,10 +6,14 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import router from './router'
 import '@/assets/css/main.css';
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi', // This is already the default value - only for display purposes
+  },
 })
 
 createApp(App).use(router).use(vuetify).mount('#app')
--- bushidaikan-vue-cap/src/views/QuizPosturasSelecionarView.vue
+++ bushidaikan-vue-cap/src/views/QuizPosturasSelecionarView.vue
@@ -2,10 +2,17 @@
   <v-container class="h-screen bg-padrao">
     <v-row no-gutters>
       <v-col align="left" cols="12">
-        <v-icon
-          end
-          icon="mdi-checkbox-marked-circle"
-        ></v-icon>
+        <v-btn
+          size="x-medium"
+          class="btn btn-bushi"
+          to="/"
+          prepend-icon="mdi-arrow-left"
+        >Voltar</v-btn>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col cols="12">
+        <v-checkbox label="Checkbox"></v-checkbox>
       </v-col>
     </v-row>
   </v-container>
@@ -18,6 +25,7 @@ export default {
   components: {
   },
   methods: {
+    
   }
 }
 </script>
